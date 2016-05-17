217dc92428bc9441bb03be59fcfb9654394f2087
Eric McKay <eric@andcode.co>
2016-05-17 22:16:23 +0000

    remove state and handler from Item

--- src/components/item.js
+++ src/components/item.js
@@ -1,14 +1,9 @@
 import React from 'react';
 
 export class Item extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { complete: this.props.complete };
-    }
     render() {
         return (
-            <div className={ this.state.complete ? 'item complete' : 'item incomplete' }
-                 onClick={ () => this.setState({complete: !this.state.complete })} >
+            <div className={ this.props.complete ? 'item complete' : 'item incomplete' }>
                 { this.props.content }
             </div>
         );
